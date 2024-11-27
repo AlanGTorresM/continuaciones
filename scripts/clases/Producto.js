dd74afcc2dfd4b4ca64fc2db4f7a3bf48c93d7a2
scorpionalan <alan_torres_mora@comunidad.unam.mx>
2024-11-27 04:44:05 -0600

    Creación de clases/Orden en código

---
+++ scripts/clases/Producto.js
@@ -0,0 +1,9 @@
+class Producto{
+    constructor(nombre, precio, descripcion, stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.images = new Array();
+    }
+}
